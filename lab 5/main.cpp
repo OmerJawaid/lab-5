296d1acdbb53cf67c021c25cdb907fe8bbe776ad
Muhammad Omer Jawaid <146324153+OmerJawaid@users.noreply.github.com>
2024-03-19 19:14:16 +0500

    Updated Book class and add book function with some more information

--- lab 5/main.cpp
+++ lab 5/main.cpp
@@ -4,22 +4,30 @@
 using namespace std;
 
 class Book {
-	string name, book_no;
+	string name, book_no, author;
+	int book_quantity;
 public:
-	Book(string na, string bn) : name(na), book_no(bn) {};
+	Book(string na, string bn,string auth,int quantity) : name(na), book_no(bn), author(auth), book_quantity(quantity){};
 	string getName() { return name; }
 	string getBookNo() { return book_no; }
+	string getAuthor() { return author; }
+	int getQuantity() { return book_quantity; }
 };
 
 vector<Book> library;
 
 void add_book() {
-	string name, book_no;
+	string name, book_no,author;
+	int quantity;
 	cout << "Enter the book name: ";
 	cin >> name;
 	cout << "Enter the book no: ";
 	cin >> book_no;
-	library.push_back(Book(name, book_no));
+	cout << "Enter author name: ";
+	cin >> author;
+	cout << "Enter book quantity: ";
+	cin >> quantity;
+	library.push_back(Book(name, book_no,author,quantity));
 	cout << "Book added successfully" << endl;
 }
 
